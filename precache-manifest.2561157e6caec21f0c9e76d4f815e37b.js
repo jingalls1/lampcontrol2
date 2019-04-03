e699ac7a2f8020cf9ef45a46c99bad1943ec97e7
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-03 01:06:04 -0700

    Updates

---
+++ precache-manifest.2561157e6caec21f0c9e76d4f815e37b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "048e2c744d621c7e7c16",
+    "url": "/jingalls1/lampcontrol2/static/js/runtime~main.f082c97a.js"
+  },
+  {
+    "revision": "3240b462967ba1436750",
+    "url": "/jingalls1/lampcontrol2/static/js/main.f1cae52b.chunk.js"
+  },
+  {
+    "revision": "7f05c87645c0f74244bd",
+    "url": "/jingalls1/lampcontrol2/static/js/2.b8f26e2f.chunk.js"
+  },
+  {
+    "revision": "3240b462967ba1436750",
+    "url": "/jingalls1/lampcontrol2/static/css/main.557aecbc.chunk.css"
+  },
+  {
+    "revision": "c3c610a01e6d4e3c3f56802b89c54b2c",
+    "url": "/jingalls1/lampcontrol2/index.html"
+  }
+];
